5c06e4bc611274d60cc578153962d6bbc20379f7
Aluno (local) <you@example.com>
2017-09-05 20:37:20 -0300

    Search end delete key in tree

--- Arvores de dados(BST)/tree.rb
+++ Arvores de dados(BST)/tree.rb
@@ -31,6 +31,66 @@ def new_node(tree, key)
   tree
 end
 
+def search(tree, key)
+	while(true)
+    unless tree
+      return nil
+    end
+
+    if tree.key == key
+      return tree
+    end
+
+    if tree.key > key
+      tree = tree.left
+
+    elsif tree.key <= key  
+		  tree = tree.right
+    end
+	end
+end
+
+def minValueNode(tree)
+  currentNode = tree
+
+  while correntNode.left != nil
+    currentNode = currentNode.left
+  end
+
+  return currentNode
+end
+
+def delete(tree, key)
+  unless tree
+    return tree
+  end
+
+  if tree.key > key
+    tree.left = delete(tree.left, key)
+  elsif tree.key < key
+    tree.right = delete(tree.right, key)
+  else
+    unless tree.left
+      tree_temp = tree.right
+      tree = nil
+      return tree_temp
+    end
+    if tree.right == nil
+      tree_temp = tree.left
+      tree = nil
+      return treem_temp
+    end
+
+    tree_temp = minValueNode(tree.right)
+
+    tree.key = tree_temp.key
+
+    tree.right = delete(tree.right, tree_temp.key)
+  end
+
+  return tree
+end
+
 def create_tree(number_node, tree)
   array = []
   counter = 1
@@ -51,10 +111,16 @@ def create_tree(number_node, tree)
   tree
 end
 
-# tree = []
-tree = create_tree(5, tree)
+tree = create_tree(10, tree)
 
 puts "Raiz: #{tree.key}"
-if tree.left
-  puts "#{tree.left.key}"
+
+value = search(tree, 6)
+
+if value != nil
+  puts "Search: #{value.key}"
 end
+
+puts "key delete: " 
+key = gets.to_i
+puts delete(tree, 6)
